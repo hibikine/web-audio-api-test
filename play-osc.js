8021212c2f336631e73533cb2f181fdd09a17731
Hibikine Kage <goodsmile314159@gmail.com>
2017-12-03 23:46:03 +0900

    AとRとFreq追加

--- play-osc.js
+++ play-osc.js
@@ -1,16 +1,21 @@
 // AudioContextを取得
 window.AudioContext = window.webkitAudioContext || window.AudioContext;
-const audioctx = new AudioContext();
+const audioCtx = new AudioContext();
 
 let play = false;
 let osc;
 let gain = 0.5;
+let gainCtx;
 let attack = 0.5;
-let decay = 0.5;
+let release = 0.5;
+let freq = 440;
 
 document.getElementById('gain').onchange = (e) => {
   console.log(e.target.valueAsNumber);
-  gain.gain.value = e.target.valueAsNumber / 100;
+  gain = convertRangeValue(e);
+  if (typeof gainCtx !== 'undefined') {
+    gainCtx.gain.value = gain;
+  }
 };
 
 function convertRangeValue(e) {
@@ -21,18 +26,27 @@ document.getElementById('attack').onchange = (e) => {
   attack = convertRangeValue(e);
 }
 
-document.getElementById('decay').onchange = (e) => {
-  decay = convertRangeValue(e);
+document.getElementById('release').onchange = (e) => {
+  release = convertRangeValue(e);
+}
+
+document.getElementById('freq').onchange = (e) => {
+  freq = convertRangeValue(e) * 950 + 50; // 20Hz~1000Hzまでマッピングしてみる
+  if (typeof osc !== 'undefined') {
+    osc.frequency.value = freq;
+  }
 }
 
 function playOsc(waveformType = 'sine') {
   if (!play) {
-    osc = audioctx.createOscillator();
-    gain = audioctx.createGain();
-    osc.connect(gain);
-    gain.connect(audioctx.destination);
-    osc.frequency.value = 440;
-    gain.gain.value = 0.8;
+    osc = audioCtx.createOscillator();
+    gainCtx = audioCtx.createGain();
+    osc.connect(gainCtx);
+    gainCtx.connect(audioCtx.destination);
+    osc.frequency.value = freq;
+    gainCtx.gain.value = 0;
+    console.log(gainCtx);
+    gainCtx.gain.linearRampToValueAtTime(gain, audioCtx.currentTime + attack);
     osc.start();
     play = true;
   }
@@ -41,7 +55,7 @@ function playOsc(waveformType = 'sine') {
 
 function stopOsc() {
   if (play) {
-    osc.stop();
+    gainCtx.gain.linearRampToValueAtTime(0, audioCtx.currentTime + release);
     play = false;
   }
 }
